807d6e963c586523a4d2110c2bc00af7f6f85db7
Oka Wibawa <okaa.wibawa@gmail.com>
2021-08-27 16:57:36 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [joke, setJoke] = useState([]);
+  const [query, setQuery] = useState();
+
+  useEffect(() => {
+    const fetchJoke = async () => {
+      const result = await axios.get(
+        query !== undefined
+          ? `https://official-joke-api.appspot.com/jokes/${query}/random`
+          : 'https://official-joke-api.appspot.com/random_joke'
+      );
+      setJoke(result.data);
+    };
+
+    fetchJoke();
+  }, [query]);
+
+  console.log(joke[0]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="Title">random jokes generator.</h1>
+
+        <p>i don't know, go fun yourself.</p>
       </header>
+
+      <main className="Main">
+        {joke.length > 0 || joke ? (
+          <>
+            <label for="cars">Choose a joke type: </label>
+
+            <select name="jokes" id="jokes" onChange={(e) => setQuery(e.target.value)}>
+              <option value="general">Random</option>
+              <option value="programming">Programming</option>
+              <option value="knock-knock">Knock Knock</option>
+            </select>
+
+            {joke ? (
+              query ? (
+                joke[0] !== undefined ? (
+                  <div className="joke-container">
+                    <h3>{joke[0].setup ? joke[0].setup : ''}</h3>
+                    <br />
+                    <br />
+                    <h3>{joke[0].punchline ? joke[0].punchline : ''}</h3>
+                  </div>
+                ) : (
+                  <h3 className="joke-container">Please wait ...</h3>
+                )
+              ) : (
+                <div className="joke-container">
+                  <h3>{joke.setup ? joke.setup : ''}</h3>
+                  <br />
+                  <br />
+                  <h3>{joke.punchline ? joke.punchline : ''}</h3>
+                </div>
+              )
+            ) : (
+              <h3 className="joke-container">Please wait ...</h3>
+            )}
+          </>
+        ) : (
+          <h3 className="joke-container">Please wait ...</h3>
+        )}
+      </main>
     </div>
   );
 }
